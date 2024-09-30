c1843bd521a089b6ed5709a51a5f9d9b52c5b6d6
meltyness <86126050+meltyness@users.noreply.github.com>
2024-09-30 14:56:44 -0500

    Improve readability

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,12 @@
 int main() {
     std::string text = "google.com.com";
     
-    const std::string rfc_1035_label     =              "(([[:alnum:]][-[:alnum:]]{1,61}[[:alnum:]])|([[:alnum:]]{1,2}))";
-    const std::string rfc_1035_subdomain = std::vformat("({}[.])", std::make_format_args(rfc_1035_label));
-    const std::string rfc_1035_regex     = std::vformat("^({}*{})$", std::make_format_args(rfc_1035_subdomain, rfc_1035_label));
+    const std::string rfc_1035_label     =              
+        "(([[:alnum:]][-[:alnum:]]{1,61}[[:alnum:]])|([[:alnum:]]{1,2}))";
+    const std::string rfc_1035_subdomain = 
+        std::vformat("({}[.])", std::make_format_args(rfc_1035_label));
+    const std::string rfc_1035_regex     = 
+        std::vformat("^({}*{})$", std::make_format_args(rfc_1035_subdomain, rfc_1035_label));
     
     
     RE2 regex(rfc_1035_regex);
